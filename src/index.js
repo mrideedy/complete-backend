c0bd92adbf5d7f222005ef306d0e701597fb5622
mrideedy <bca40584.20@bitmesra.ac.in>
2024-11-30 17:36:23 +0530

    mongo atlas connection

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "/.env"
+})
+connectDB()
+
+// const app = express()(async () => {
+//   try {
+//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// });
