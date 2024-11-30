32d0635a5bd1e9135516c490ada291a1302dc178
mrideedy <bca40584.20@bitmesra.ac.in>
2024-12-01 01:17:07 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import express from 'express'
 dotenv.config({
-    path: "/.env"
-})
+  path: "/.env",
+});
+
+const app = express()
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error: ", err);
+  });
 
 // const app = express()(async () => {
 //   try {
